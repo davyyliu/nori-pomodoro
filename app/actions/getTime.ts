cf360d7acf88ee049f9b84567c9697ec3c573e81
davyyliu <61434358+davyyliu@users.noreply.github.com>
2023-05-27 15:51:58 -0700

    sync account to time

--- app/actions/getTime.ts
+++ app/actions/getTime.ts
@@ -1,9 +1,20 @@
 import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "./getCurrentUser";
 
 export default async function getTime() {
   try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      return [];
+    }
+
     const timeSpecs = await prisma.pomodoroHistory.findFirst({
-      where: {},
+      where: {
+        userId: {
+          in: currentUser.id,
+        },
+      },
       orderBy: { createdAt: "desc" },
     });
 
