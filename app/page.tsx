fb0f966a609f2859c28349247c3c2d649f88a3a8
davyyliu <61434358+davyyliu@users.noreply.github.com>
2023-05-28 00:14:13 -0700

    sync time and user

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ import getTime from "@/app/actions/getTime";
 import PomodoroTimer from "./components/timer/PomodoroTimer";
 
 const Home = async () => {
-  const time = await getTime();
+  let time = await getTime();
 
   return (
     <ClientOnly>
       <div className="overflow-hidden">
         <PomodoroTimer
-          studyhrs={Number(time?.studyhours) || 0}
-          studymins={Number(time?.studyminutes) || 0}
+          studyhrs={Array.isArray(time) ? 0 : Number(time?.studyhours) || 0}
+          studymins={Array.isArray(time) ? 0 : Number(time?.studyminutes) || 0}
           studysecs={0}
-          breakhrs={Number(time?.breakhours) || 0}
-          breakmins={Number(time?.breakminutes) || 0}
+          breakhrs={Array.isArray(time) ? 0 : Number(time?.breakhours) || 0}
+          breakmins={Array.isArray(time) ? 0 : Number(time?.breakminutes) || 0}
           breaksecs={0}
-          sess={Number(time?.sessions) || 1}
+          sess={Array.isArray(time) ? 0 : Number(time?.sessions) || 0}
         />
       </div>
     </ClientOnly>
