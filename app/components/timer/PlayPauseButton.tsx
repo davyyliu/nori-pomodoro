26039d10b7821d5563606b4bf5dc3fcf9f6f3a5b
davyyliu <61434358+davyyliu@users.noreply.github.com>
2023-05-29 03:01:32 -0700

    try visiblity

--- app/components/timer/PlayPauseButton.tsx
+++ app/components/timer/PlayPauseButton.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  PlayCircleIcon,
-  PauseCircleIcon,
-  AdjustmentsHorizontalIcon,
-} from "@heroicons/react/24/outline";
+import { PlayCircleIcon, PauseCircleIcon } from "@heroicons/react/24/outline";
 
 interface PlayPauseButtonProps {
   onClick: () => void;
@@ -31,7 +27,7 @@ const PlayPauseButton: React.FC<PlayPauseButtonProps> = ({
     >
       {clicked ? (
         <button className="playbtn" {...props} onClick={onClick}>
-          <AdjustmentsHorizontalIcon
+          <PauseCircleIcon
             className={`
             xs:h-20
             xs:w-20
@@ -45,13 +41,12 @@ const PlayPauseButton: React.FC<PlayPauseButtonProps> = ({
             xl:w-16
             2xl:h-24
             2xl:w-24
-            stroke-width-1.5 stroke-[#F0E6D4] fill-none
-            z-50`}
+            stroke-width-1.5 stroke-[#F0E6D4] fill-none`}
           />
         </button>
       ) : (
         <button className="playbtn" {...props} onClick={onClick}>
-          <AdjustmentsHorizontalIcon
+          <PlayCircleIcon
             className={`
             xs:h-20
             xs:w-20
@@ -65,8 +60,7 @@ const PlayPauseButton: React.FC<PlayPauseButtonProps> = ({
             xl:w-16
             2xl:h-24
             2xl:w-24
-          stroke-width-1.5 stroke-[#4E7563] fill-none
-          z-50`}
+          stroke-width-1.5 stroke-[#4E7563] fill-none`}
           />
         </button>
       )}
